eb1237889bc8cc5150acf57da4ed3446185c3fd2
wtc <bematjhb025@student.wethinkcode.co.za>
2026-08-17 16:49:31 +0200

    added FraudSeerviceApplicion.java
    created fraud application service main entry pooint for spring boot

---
+++ fraud-service/fraud-service/src/main/java/com/transactalert/fraud/FraudServiceApplication.java
@@ -0,0 +1,11 @@
+package com.transactalert.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudServiceApplication {
+    public static void main(String[] args) {
+        SpringBootApplication.run(FraudServiceApplication.class, args);
+    }
+}
