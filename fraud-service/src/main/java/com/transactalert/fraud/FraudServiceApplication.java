9f4c98dc66e671553cacf08f3831b6855ebd7262
benitmatumona <bematjhb025@student.wethinkcode.co.za>
2026-08-17 20:52:15 +0200

    added EnableJms to FraudServiceApplication
    added annotation and import statement

--- fraud-service/src/main/java/com/transactalert/fraud/FraudServiceApplication.java
+++ fraud-service/src/main/java/com/transactalert/fraud/FraudServiceApplication.java
@@ -2,8 +2,10 @@ package com.transactalert.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class FraudServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudServiceApplication.class, args);
