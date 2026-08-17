33608b5d99e8d4b1b6050f9db7eb01cce1670b84
benitmatumona <bematjhb025@student.wethinkcode.co.za>
2026-08-17 23:15:49 +0200

    added producer and pom file file notification-service
    changed fraud consmer not just to print but to use producer to send alerts to actual queues

--- fraud-service/src/main/java/com/transactalert/fraud/consumer/FraudConsumer.java
+++ fraud-service/src/main/java/com/transactalert/fraud/consumer/FraudConsumer.java
@@ -4,22 +4,40 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 import com.transactalert.common.event.TransactionEvent;
 import com.transactalert.fraud.rules.FraudRuleEngine;
+import com.transactalert.common.event.FraudAlertEvent;
+import com.transactalert.fraud.producer.FraudAlertProducer;
 
 
 @Component
 public class FraudConsumer{
     private final FraudRuleEngine fraudRuleEngine;
-    
-    public FraudConsumer(FraudRuleEngine fraudRuleEngine){
+    private final FraudAlertProducer fraudAlertProducer;    
+
+    public FraudConsumer(
+        FraudRuleEngine fraudRuleEngine, 
+        FraudAlertProducer fraudAlertProducer
+    ){
         this.fraudRuleEngine = fraudRuleEngine;
+        this.fraudAlertProducer = fraudAlertProducer;
     }
 
     @JmsListener(destination = "fraud.queue")
     public void receiveTransaction(TransactionEvent event){
         int score = fraudRuleEngine.calculateRiskScore(event);
-        if (score >= 50) System.out.println(
+        if (score >= 50){
+            System.out.println(
             "ALERT: Transaction " + event.getTransactionId() + " flagged with risk score " + score
-        );
+            );
+            FraudAlertEvent alertEvent = new FraudAlertEvent(
+                event.getTransactionId(),
+                event.getCustomerId(), 
+                event.getAmount(), 
+                event.getMerchant(), 
+                event.getLocation(),
+                score
+            );
+            fraudAlertProducer.sendAlert(alertEvent);
+        }
         else System.out.println(
              "Transaction " + event.getTransactionId() + " passed fraud check"
         );
